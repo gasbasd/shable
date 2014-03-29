84bad7562c08668a24299606db63d1598282203a
Andrei Neagu <andrey_2002_eu@yahoo.it>
2014-03-29 15:28:24 +0100

    added user location

--- shable/controllers/user_profile/location.py
+++ shable/controllers/user_profile/location.py
@@ -1,10 +1,61 @@
-from tg import expose
+# coding=utf-8
+from __future__ import unicode_literals
+from tg import expose, lurl, predicates, redirect, flash, request
+from tw2.forms import ListForm, TextField, SingleSelectField, SubmitButton, TextArea, CheckBoxList
 from shable.lib.base import BaseController
 
 
-class LocationProfileController(BaseController):
+class LocationProfileForm(ListForm):
+    css_class = 'shable-form'
+    description = TextArea(label='Descrizione breve', css_class='form-control')
+    #details = TextArea(label='Dettagli su non so cosa', css_class='form-control')
+    address = TextField(label='Indirizzo', css_class='form-control')
+    number = TextField(label='Numero', css_class='form-control')
+    city = TextField(label='Città', css_class='form-control')
+    zip_code = TextField(label='CAP', css_class='form-control')
+    food_types = CheckBoxList(label='Preferenze cibi', css_class='form-control',  options = ['Pasta', 'Carne', 'Pizza', 'Desert', 'Birra'])
+    preferences = CheckBoxList(label='Preferenze alimentazione', css_class='form-control',  options = ['Vegano', 'No glutine'])
+    #add a photo
 
-    @expose('json')
+    submit = SubmitButton(value='Salva Location', css_class='form-control')
+    action = lurl('/user_profile/location/update_details')
+
+
+class LocationProfileController(BaseController):
+    allow_only = predicates.not_anonymous()
+
+    @expose('shable.templates.user_profile.location')
     def index(self):
-        return {}
+        user = request.identity['user']
+        return {'form': LocationProfileForm, 'value': user.location}
+
+    @expose()
+    def update_details(self, **kw):
+        user = request.identity['user']
+        user.location.description = kw['description']
+        user.location.address = kw['address']
+        user.location.number = kw['number']
+        user.location.city = kw['city']
+        user.location.zip_code = kw['zip_code']
+
+        if kw.get('food_types') is not None:
+            if not isinstance(kw.get('food_types'),list):
+                user.location.food_types = [kw.get('food_types')]
+            else:
+                user.location.food_types = kw.get('food_types')
+        else:
+            user.location.food_types = []
+
+        if kw.get('preferences') is not None:
+            if not isinstance(kw.get('preferences'),list):
+                user.location.preferences = [kw.get('preferences')]
+            else:
+                user.location.preferences = kw.get('preferences')
+        else:
+            user.location.preferences = []
+
+
+
+        flash('Location salvata')
+        redirect('/user_profile/location')
 
